3e4805e5d24578ce4033f6e8e570c67603ebb993
JM <jdmalcolm1602@gmail.com>
2024-10-30 16:55:40 -0400

    added endpoint to update an account

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -151,3 +151,24 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(len(data), num_accounts)
+
+    def test_update_account(self):
+        """It should Update an existing Account"""
+        # create an Account to update
+        test_account = AccountFactory()
+        resp = self.client.post(BASE_URL, json=test_account.serialize())
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+        # update the account
+        new_account = resp.get_json()
+        new_account["name"] = "Something Known"
+        resp = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        updated_account = resp.get_json()
+        self.assertEqual(updated_account["name"], "Something Known")
+    
+    def test_update_account_not_found(self):
+        """It should throw 404 if account not found on Update"""
+        dummy_account = AccountFactory()
+        resp = self.client.put(f"{BASE_URL}/0", json=dummy_account.serialize())
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
